553ded6f19a149f3cad6f5c23ba8847e28c2dd4a
OWAIS <120783442+owais-alhjri@users.noreply.github.com>
2026-07-26 00:53:25 +0400

    Now the asset detail page  works, but it will need some changes later

--- AssetFlow-frontend/src/app/features/assets/asset-detail/asset-detail.ts
+++ AssetFlow-frontend/src/app/features/assets/asset-detail/asset-detail.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { AssetsServices } from '../../../core/services/assetsServices';
+import { ActivatedRoute } from '@angular/router';
+import { Asset } from '../../../shared/models/asset.model';
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-asset-detail',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './asset-detail.html',
   styleUrl: './asset-detail.scss',
 })
-export class AssetDetail {}
+export class AssetDetail implements OnInit {
+  private assetServices = inject(AssetsServices);
+  private route = inject(ActivatedRoute);
+
+  assetId: string = '';
+  asset = signal<Asset | null>(null);
+  ngOnInit(): void {
+    this.loadAssetById();
+  }
+
+  loadAssetById(){
+    this.assetId = this.route.snapshot.paramMap.get('id') || '';
+
+    this.assetServices.getAssetById(this.assetId).subscribe({
+      next: (res) =>{
+        this.asset.set(res)
+      }
+    });
+  }
+}
